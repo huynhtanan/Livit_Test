4016b0464bbf5dfadd8ef50e93fa349505b5bae6
huynhtanan <huynhtanan90@gmail.com>
2017-01-04 11:30:04 +0700

    last commit

--- src/Abc.Dal/Models/DatabaseContext.cs
+++ src/Abc.Dal/Models/DatabaseContext.cs
@@ -60,13 +60,13 @@ namespace Abc.Dal.Models
 					if (entry.State == EntityState.Added)
 					{
 						entity.CreatedDate = now;
-						base.Entry(entity).Property(x => x.UpdatedDate).IsModified = true;
 					}
 					else
 					{
-						entity.UpdatedDate = now;
-						base.Entry(entity).Property(x => x.UpdatedDate).IsModified = true;
+						base.Entry(entity).Property(x => x.CreatedDate).IsModified = false;
 					}
+					
+					entity.UpdatedDate = now;
 				}
 			}
 
